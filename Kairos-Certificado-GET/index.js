e7a5d215fc8751ee306c7c29930819a39d2937db
Luizpfarias <lprfarias@gmail.com>
2019-06-28 17:38:53 -0300

    Add files via upload

---
+++ Kairos-Certificado-GET/index.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const axios = require('axios');
+const uniqid = require('uniqid');
+
+exports.handler = async (event, context, callback) => {
+	
+	let errorMessage = null;
+	let results = {};
+	
+	await axios.get(`https:<firebase-url>/certificado/${event.id}.json`)
+		.then(res => {
+            results = {...res.data};
+		})
+		.catch(getError => {
+			errorMessage = getError;
+		})
+	
+	if (!errorMessage) callback(null, results);
+	callback(errorMessage);
+
+};
